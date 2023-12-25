63e38a7925b1a91ed641d9172f3a32fadf6b1be6
Rabiul hossain. <mohammadrabiul4@gmail.com>
2023-12-25 17:22:29 +0600

    usePlaylist custom hook implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from 'react'
 import getPlaylist from './api'
+import usePlaylist from './hooks/usePlaylist';
 
 
 function App() {
+  const {getPlaylistById, playlists} = usePlaylist();
+  
   useEffect(()=> {
-    getPlaylist('PL_XxuZqN0xVD0op-QDEgyXFA4fRPChvkl').then((res => console.log(res)));
+    getPlaylistById('PL_XxuZqN0xVD0op-QDEgyXFA4fRPChvkl');
   }, [])
+
+  console.log('Playlists', playlists);
   return (
     <>
       <h3>Clean YouTube</h3>
